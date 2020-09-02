a78d897a4bb6416c5c6cdc600f14f4f3d9d08fac
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-09-02 17:21:13 +0200

    Added a rankings-page and fixed route
    Added some style but not done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SpottingStatsComponent } from './pages/spotting-stats/spotting-stats.co
 import { LeastSpottedPokemonsComponent } from './components/least-spotted-pokemons/least-spotted-pokemons.component';
 import { BestPlayersComponent } from './components/best-players/best-players.component';
 import { OverallRankComponent } from './components/overall-rank/overall-rank.component';
+import { SpottingRankingsComponent } from './pages/spotting-rankings/spotting-rankings.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { OverallRankComponent } from './components/overall-rank/overall-rank.com
     SpottingStatsComponent,
     LeastSpottedPokemonsComponent,
     BestPlayersComponent,
-    OverallRankComponent
+    OverallRankComponent,
+    SpottingRankingsComponent
   ],
   imports: [
     BrowserModule,
