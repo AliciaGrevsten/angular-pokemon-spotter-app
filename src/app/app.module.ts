60dd8898d1fcc979da6f4a6cc76c4ed7be8e3787
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-09-02 07:37:07 +0200

    Added an arraylist of players

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterPokemonComponent } from './components/register-pokemon/register-pokemon.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { BestPlayersComponent } from './components/best-players/best-players.component';
+import { OverallRankComponent } from './components/overall-rank/overall-rank.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterPokemonComponent,
-    NavbarComponent
+    NavbarComponent,
+    BestPlayersComponent,
+    OverallRankComponent
   ],
   imports: [
     BrowserModule,
