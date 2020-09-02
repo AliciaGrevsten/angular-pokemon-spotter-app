4aeeb90c7b32c3df64c8e0e33d395b6f06ba85be
Josefine Salomaa <josefine.salomaa@se.experis.com>
2020-09-02 16:02:21 +0200

    first version of reg pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterPokemonComponent } from './components/register-pokemon/register-pokemon.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NavbarComponent } from './components/layout/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register-pokemon/register-pokemon.component.ts
+++ src/app/components/register-pokemon/register-pokemon.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register-pokemon',
   templateUrl: './register-pokemon.component.html',
-  styleUrls: ['./register-pokemon.component.scss']
+  styleUrls: ['./register-pokemon.component.scss'],
+  
 })
 export class RegisterPokemonComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+  data:any = []
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getName()
   }
 
+  getName() {
+    const url ='https://pokeapi.co/api/v2/pokemon?limit=151&offset=0/'
+    this.http.get(url).pipe(
+      map((data:any) => data.results)
+    ).subscribe((res)=>{
+      this.data = res
+      console.log(this.data)
+      })
+  }
+  
+  
 }
